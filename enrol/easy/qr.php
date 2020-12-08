aae498a9b6d45a8e5fed563e8d4a0e9da2ab7fe7
Felixod <gorbatovsv@gmail.com>
2020-12-08 10:03:58 +0400

    enrol_easy update to 1.7

--- enrol/easy/qr.php
+++ enrol/easy/qr.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Запись с помощью QR-кода</title>
+    <title>Enroll via QR Code</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
   <body>
